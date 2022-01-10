21672dc09b4d6ce78c56f70705a5ecbcfed8c8b1
Gabriel Escame <gabriel.escame@gmail.com>
2022-01-10 02:04:05 -0300

    refactor: add makeSut

--- src/data/usecases/lyrics/lyrics-service.spec.ts
+++ src/data/usecases/lyrics/lyrics-service.spec.ts
@@ -1,11 +1,24 @@
 import { HttpGetClientSpy } from '../../mocks/mock-http-client'
 import { LyricsService } from './lyrics-service'
 
+type SutTypes = {
+  sut: LyricsService
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new LyricsService(url, httpGetClientSpy)
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('LyricsService', () => {
   test('Should call HttpGetClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new LyricsService(url, httpGetClientSpy)
+    const url = 'other_url'
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.search()
     expect(httpGetClientSpy.url).toEqual(url)
   })
