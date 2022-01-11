48ede2d047aeb6a4555f7db10f05bc8b201b317e
Gabriel Escame <gabriel.escame@gmail.com>
2022-01-11 00:38:26 -0300

    test: lyrics service generate with faker url

--- src/data/usecases/lyrics/lyrics-service.spec.ts
+++ src/data/usecases/lyrics/lyrics-service.spec.ts
@@ -1,12 +1,14 @@
 import { HttpGetClientSpy } from '../../mocks/mock-http-client'
 import { LyricsService } from './lyrics-service'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const faker = require('faker')
 
 type SutTypes = {
   sut: LyricsService
   httpGetClientSpy: HttpGetClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpGetClientSpy = new HttpGetClientSpy()
   const sut = new LyricsService(url, httpGetClientSpy)
   return {
@@ -17,7 +19,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe('LyricsService', () => {
   test('Should call HttpGetClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpGetClientSpy } = makeSut(url)
     await sut.search()
     expect(httpGetClientSpy.url).toEqual(url)
