a2ff27edf48ef781e099fcb2625e2cc4c1c49109
Gabriel Escame <gabriel.escame@gmail.com>
2022-01-29 20:04:26 -0300

    test: adding params to search

--- src/data/usecases/lyrics/lyrics-service.spec.ts
+++ src/data/usecases/lyrics/lyrics-service.spec.ts
@@ -19,8 +19,11 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 describe('LyricsService', () => {
   test('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
+    const artist = faker.name.firstName()
+    const title = faker.music.genre()
+    const finalUrl = `${url}/${artist}/${title}`
     const { sut, httpGetClientSpy } = makeSut(url)
-    await sut.search()
-    expect(httpGetClientSpy.url).toEqual(url)
+    await sut.search({ artist, title })
+    expect(httpGetClientSpy.url).toEqual(finalUrl)
   })
 })
