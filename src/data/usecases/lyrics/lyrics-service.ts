fed8a13552fafd58d18dbcc1360d7a87ee6b400d
Gabriel Escame <gabriel.escame@gmail.com>
2022-02-01 01:11:51 -0300

    feat: ensure LyricsService returns a lyric on sucess

--- src/data/usecases/lyrics/lyrics-service.ts
+++ src/data/usecases/lyrics/lyrics-service.ts
@@ -3,20 +3,21 @@ import { httpStatusCode } from '@/data/protocols/http/http-response'
 import { NotFoundError } from '@/domain/errors/not-found-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { LyricsModel, LyricsParams } from '@/domain/models'
+import { Lyrics } from '@/domain/usecases/lyrics'
 
-export class LyricsService {
+export class LyricsService implements Lyrics {
   constructor(
     private readonly url: string,
     private readonly httpGetClient: HttpGetClient<LyricsModel>
   ) {}
 
-  async search(params: LyricsParams): Promise<void> {
+  async search(params: LyricsParams): Promise<LyricsModel> {
     const url = `${this.url}/${params?.artist}/${params?.title}`
     const httpResponse = await this.httpGetClient.get({ url })
 
     switch (httpResponse.statusCode) {
       case httpStatusCode.ok:
-        break
+        return httpResponse.body
       case httpStatusCode.notFound:
         throw new NotFoundError()
       default:
